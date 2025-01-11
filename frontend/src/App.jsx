4ed67763fd4717054fde80a23ab361065b040e30
ahmeddaruwala <ahmeddaruwala360@gmail.com>
2025-01-11 11:12:46 +0500

    updated todo app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,14 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 
-const maxTodoLength = 30;
+const maxTodoLength = 50;
 
 function App() {
   const BASE_URL =
-    "https://to-do-app-express-iela.vercel.app" || "http://localhost:3000";
+    "http://localhost:3000";
   const [todos, setTodos] = useState();
   const getTodo = async () => {
+    
     try {
       const res = await axios.get(`${BASE_URL}/api/v1/todos`);
       if (res?.data?.data) {
@@ -25,6 +26,7 @@ function App() {
 
   useEffect(() => {
     getTodo();
+    
   }, []);
 
   const addTodo = async (e) => {
@@ -35,10 +37,12 @@ function App() {
       return;
     }
     try {
+      
       if (todoValue !== "") {
         const res = await axios.post(`${BASE_URL}/api/v1/todo`, {
           todocontent: todoValue,
         });
+  
         setTodos((previoustodos) => [...previoustodos, res?.data?.data]);
         e.target.children[0].value = "";
       }
@@ -49,6 +53,7 @@ function App() {
   const deleteTodo = async (id) => {
     try {
       const res = await axios.delete(`${BASE_URL}/api/v1/todo${id}`);
+      
       if (res?.status == 201) {
         setTodos((previoustodos) =>
           previoustodos.filter((todo) => todo.id !== id)
@@ -57,52 +62,130 @@ function App() {
       toast(res.data?.message, {
         icon: "🗑️",
         style: {
-          borderRadius: "10px",
-          background: "#333",
-          color: "#fff",
+          borderRadius: "8px",
+          background: "#032470",
+          color: "#71b3ff",
         },
       });
     } catch (error) {
-      console.log("error delete in todo", error);
+      console.log("error deleting todo", error);
+    }
+  };
+  const editTodo = async (event, id) => {
+    event.preventDefault();
+    const todoValue = event.target.children[0].value;
+    if (todoValue.trim() === "") {
+      toast("Todo Can't be empty", {
+        icon: "❌",
+        style: {
+          borderRadius: "8px",
+          background: "#1261a0",
+          color: "#3895d3",
+        },
+      });
+      return;
+    }
+    try {
+      await axios.patch(`${BASE_URL}/api/v1/todo${id}`, {
+        todocontent: todoValue,
+      });
+      setTodos((previoustodos) => {
+        return previoustodos.map((todo) => {
+          if (todo.id == id) {
+            return { ...todo, todocontent: todoValue, isEditing: false };
+          }
+          return todo;
+        });
+      });
+    } catch (error) {
+      console.error("Error editing todo:", error);
     }
   };
 
   return (
     <>
-      <div className="min-h-screen bg-red-900 text-white flex flex-col justify-center items-center">
-        <h1 className="text-4xl font-bold text-blue-400 mb-6">To-Do App</h1>
-        <div className="w-full sm:w-96 bg-gray-800 p-6 rounded-lg shadow-lg ">
+      <div className="min-h-screen bg-blue-400 text-blue-400 flex flex-col items-center">
+        <h1 className="text-4xl font-bold text-blue-700 mb-6 mt-24">To-Do App</h1>
+        <div className="w-full sm:w-96 bg-blue-800 p-6 rounded-lg shadow-lg ">
           <form onSubmit={addTodo}>
-            {/* Input for new task */}
+            
             <input
               type="text"
-              className="w-full p-3 rounded-lg mb-4 bg-gray-700 text-white outline-none placeholder-gray-400"
-              placeholder="Enter task..."
+              className="w-full p-3 rounded-lg mb-4 bg-blue-400 text-black outline-none placeholder-black"
+              placeholder="Enter your task..."
             />
-            <button className="w-full py-3 bg-blue-500 text-black font-bold rounded-lg hover:bg-blue-600 transition-all">
+            <button className="w-full py-3 bg-blue-400 text-black font-bold rounded-lg hover:bg-blue-500 transition-all">
               Add Task
             </button>
-          </form>
-          {/* Add Task button */}
+          </form>  
 
-          {/* Task List */}
           <ul className="mt-4 space-y-2">
-            {todos?.map((todo) => (
+            {todos?.map((todo, index) => (
               <li
                 key={todo.id}
-                className="flex justify-between items-center p-2 bg-gray-700 rounded-lg"
+                className="flex justify-between items-center p-2 bg-blue-400 rounded-lg"
               >
-                <span className="text-white">{todo.todocontent}</span>
-                <div className="flex space-x-2">
-                  {/* <button className="text-yellow-400 hover:text-white transition-all">
-                    Edit
-                  </button> */}
-                  <button
-                    className="text-red-500 hover:text-white transition-all"
-                    onClick={() => deleteTodo(todo.id)}
+                {!todo.isEditing ? (
+                  <span className="text-black">{todo.todocontent}</span>
+                ) : (
+                  <form
+                    className="flex gap-6"
+                    onSubmit={(e) => editTodo(e, todo.id)}
                   >
-                    Delete
-                  </button>
+                    <input
+                      type="text"
+                      defaultValue={todo.todocontent}
+                      className="border border-blue-600 text-black bg-blue-400 rounded"
+                    />
+                    <div className="flex gap-3">
+                      {" "}
+                      <button
+                        onClick={() => {
+                          const newTodos = todos.map((todo) => {
+                            todo.isEditing = false;
+                            return todo;
+                          });
+                          setTodos([...newTodos]);
+                        }}
+                        type="button"
+                        className="text-red-600"
+                      >
+                        Cancel
+                      </button>
+                      <button type="submit" className="text-blue-800">
+                        Submit
+                      </button>
+                    </div>
+                  </form>
+                )}
+                <div className="flex space-x-2">
+                  {!todo.isEditing ? (
+                    <button
+                      onClick={() => {
+                        const newTodos = todos?.map((todo, i) => {
+                          if (i == index) {
+                            todo.isEditing = true;
+                          } else {
+                            todo.isEditing = false;
+                          }
+                          return todo;
+                        });
+                        setTodos([...newTodos]);
+                      }}
+                      className="text-blue-800 hover:text-black transition-all"
+                    >
+                      Edit
+                    </button>
+                  ) : null}
+
+                  {!todo?.isEditing ? (
+                    <button
+                      className="text-red-600 hover:text-black transition-all"
+                      onClick={() => deleteTodo(todo.id)}
+                    >
+                      Delete
+                    </button>
+                  ) : null}
                 </div>
               </li>
             ))}

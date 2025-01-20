dcd97d4c1d5f98b4d693be27228d17acff1e10cd
ahmeddaruwala <ahmeddaruwala360@gmail.com>
2025-01-20 18:37:16 +0500

    updated

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import 'dotenv/config'
 import './database.js'
-import { Todo } from "./models/index.js";
+import { Todo } from "./models/model.js";
 
 const app = express();
 const port = process.env.PORT || 5002;
